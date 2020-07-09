106072a8a217274e0a044005a754b6ab27cb101b
Camila Janssen <cajanssen@gmail.com>
2020-07-09 16:50:36 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -95,6 +95,32 @@ class Category extends Model {
 		}
 	}
 
+	public function getProductsPage($page = 1, $itensPerPage = 2)
+	{
+
+		$start = ($page - 1) * $itensPerPage;
+
+		$sql = new Sql();
+		
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products a
+			INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+			INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itensPerPage;
+		", [':idcategory'=>$this->getidcategory()
+		]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() as nrtotal;");
+
+		return [
+			'data'=>Product::checkList($results),
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itensPerPage)
+		];
+
+	}
+
 	public function addProduct(Product $product)
 	{
 		$sql = new Sql();
