d830806db2581d645869341e1c28d73755a01dd0
Camila Janssen <cajanssen@gmail.com>
2020-07-14 17:14:12 -0300

    Ajuste Endereço

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/3">Motorola</a></li><li><a href="/categories/5">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Android</a></li><li><a href="/categories/4">Apple</a></li><li><a href="/categories/3">Motorola</a></li><li><a href="/categories/5">Samsung</a></li><li><a href="/categories/10">Teste 1</a></li>
